22f140e4b4c747dd72bf870ed0d704f554e406ab
Theogama <sandilegama.sg@gmail.com>
2023-10-10 09:17:04 +0200

    Update files

--- app/Products/[id]/page.tsx
+++ app/Products/[id]/page.tsx
@@ -175,7 +175,7 @@ const ProductDetails = async ({ params: { id } }: Props) => {
             height={22}
           />
 
-          <Link href="/ " className="text-base text-white">
+          <Link href="/" className="text-base text-white">
             Buy Now
           </Link>
         </button>
